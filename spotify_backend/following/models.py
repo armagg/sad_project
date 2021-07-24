8b4c6318598c71f0343eee177f2b226b66824fd5
omid <omid.mesgarha@gmail.com>
2021-07-24 09:49:15 +0430

    add follow app

---
+++ spotify_backend/following/models.py
@@ -0,0 +1,11 @@
+from django.db.models.deletion import CASCADE
+from django.db.models.fields.related import ForeignKey
+from spotify_backend.accounts.models import Listener, Singer
+from django.db import models
+
+class FollowSinger(models.Model):
+    user = ForeignKey(Listener, on_delete=CASCADE)
+    singer = ForeignKey(Singer, on_delete=CASCADE)
+
+class FollowPlaylist(models.Model):
+    pass
