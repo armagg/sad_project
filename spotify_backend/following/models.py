f75814390e795332d92d45943823861b6eef05ef
omid <omid.mesgarha@gmail.com>
2021-07-25 22:56:42 +0430

    add follow playlist model

--- spotify_backend/following/models.py
+++ spotify_backend/following/models.py
@@ -1,6 +1,7 @@
 from django.db.models.deletion import CASCADE
 from django.db.models.fields.related import ForeignKey
 from accounts.models import Listener, Singer
+from playlists.models import Playlist
 from django.db import models
 
 class FollowSinger(models.Model):
@@ -8,4 +9,6 @@ class FollowSinger(models.Model):
     singer = ForeignKey(Singer, on_delete=CASCADE)
 
 class FollowPlaylist(models.Model):
-    pass
+    user = models.ForeignKey(Listener, on_delete=CASCADE)
+    playlist = models.ForeignKey(Playlist, on_delete=CASCADE)
+    
