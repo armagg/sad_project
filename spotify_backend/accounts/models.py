75ad35bb7f93fbac0a30b8abfa5d66154c20f854
omid <omid.mesgarha@gmail.com>
2021-07-25 10:06:35 +0430

    change singer type

--- spotify_backend/accounts/models.py
+++ spotify_backend/accounts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth import models as auth
 from django.db.models.deletion import CASCADE
+from django.db.models.fields.related import ManyToManyField
 
 
 FREE = 'FREE'
@@ -15,13 +16,12 @@ class User(auth.User):
 
 
 class Listener(models.Model):
-
     user = models.ForeignKey(User, on_delete=CASCADE)
     account_type = models.CharField(choices=ACC_TYPES, max_length=5)
 
 
 class Singer(models.Model):
-    user = models.ForeignKey(User, on_delete=CASCADE)
+    name = models.CharField(max_length=16, default='خواننده')
     description = models.TextField('descriptions')
     biography = models.TextField()
 
