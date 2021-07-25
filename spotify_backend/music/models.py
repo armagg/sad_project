4b40f1e5110931b0f5fa8979be62c76661a3e0ff
omid <omid.mesgarha@gmail.com>
2021-07-25 10:07:05 +0430

    add musics models

--- spotify_backend/music/models.py
+++ spotify_backend/music/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.db.models.base import Model
+from django.db.models.deletion import CASCADE, DO_NOTHING
+from accounts.models import Provider, Singer
 
-# Create your models here.
+
+POD = 'podcast'
+MUSIC = 'music'
+
+MUSIC_TYPE = [
+    (POD, 'podcast'),
+    (MUSIC, 'music')
+]
+class Music(models.Model):
+    singer = models.ManyToManyField(Singer)
+    name = models.CharField(max_length=30)
+    file_id = models.IntegerField()
+    provider = models.ForeignKey(Provider, on_delete=DO_NOTHING)
+    file_type = models.CharField(max_length=7, choices=MUSIC_TYPE)
+    lenght = models.IntegerField('time in seconds')
+    release_date = models.DateField()
+    cover = models.ImageField('cover')
+    played_count = models.IntegerField(default=0)
+
+class Genre(models.Model):
+    name = models.CharField(max_length=16)
+    description = models.TextField()
+
+class MusicGenre(models.Model):
+    music = models.ForeignKey(Music, CASCADE)
+    genre = models.ForeignKey(Genre, CASCADE)
+    weight = models.SmallIntegerField()
